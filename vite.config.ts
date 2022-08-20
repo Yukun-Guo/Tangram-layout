5a3187b309b4d1c22b59b599fe34b642b3e20dfe
Yukun <guoyukn@gmail.com>
2022-08-19 17:15:52 -0700

    update for package build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/tangram-layout/index.ts'),
+      name: 'MyLib',
+      fileName: (format) => `tangram-layout.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
